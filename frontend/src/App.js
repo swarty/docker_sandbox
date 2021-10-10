918d1161fb8cbf40f4e217916f8585bdf67f949d
swarty <swarty16@gmail.com>
2021-10-10 19:06:03 +0300

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Some new changes sdsd
+          Edit <code>src/App.js</code> and save to reload. Some new changes
         </p>
         <a
           className="App-link"
